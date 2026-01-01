3b7e39292c1fa97baab7a07fdffcffd62ed67f9e
Wesbonf <wesleybbonfim@hotmail.com>
2025-12-31 22:53:07 -0300

    3. Aleatórios.py

---
+++ 3.Fluxodecontrole/3. Aleatórios.py
@@ -0,0 +1,20 @@
+import random
+
+num = random.randint(1, 8)
+
+if num == 1:
+    print('Sim - com certeza.')
+elif num == 2:
+    print('Decididamente sim.')
+elif num == 3:
+    print('Sem dúvida.')
+elif num == 4:
+    print('Resposta incerta, tente novamente.')
+elif num == 5:
+    print('Pergunte novamente mais tarde.')
+elif num == 6:
+    print('Concentre-se e pergunte novamente.')
+elif num == 7:
+    print('Minha resposta é não.')
+elif num == 8:
+    print('Muito duvidoso.')
