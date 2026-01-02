5f184dcaf7d1f876a4fe3bb46684528ae491a643
Wesbonf <wesleybbonfim@hotmail.com>
2026-01-01 23:34:53 -0300

    2. Variáveis modulo 2

---
+++ 2.Variaveis/1. variáveis.py
@@ -0,0 +1,31 @@
+## 1. Variáveis em Python
+
+name = 'Wesley Bonfim'
+user_id = 1687856745
+progress = 0.75
+xp = 60
+verified = True
+
+
+## 2. Também podemos alterar o valor de uma variável ou imprimi-la:
+
+xp = 70
+xp = 80
+
+print(xp)  # Imprime 80
+
+
+## 3. int - número inteiro, sem vírgula decimal
+
+year = 2000
+age = 26
+
+## 4. float - número decimal com vírgula
+
+pi = 3.14159
+meal_cost= 12.99
+
+## 5. bool - True (verdadeiro) ou False (falso), usado em estruturas de controle
+
+late_to_class = False
+cranky = True
